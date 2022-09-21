91eab6b94fe8e6a0887637a79b81beeb5824817c
Наташа Ананас <konnat7@mail.ru>
2022-09-21 20:49:39 +0300

    UIKitDZ

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Анастасия Козлова on 21.09.2022.
+//
+
+import UIKit
+/// ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
