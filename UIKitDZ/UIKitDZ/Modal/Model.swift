b386f303004ae2acc766c622ee943dd1b0ceceb9
Наташа Ананас <konnat7@mail.ru>
2022-09-22 17:43:43 +0300

    DZ_3_part_2

---
+++ UIKitDZ/UIKitDZ/Modal/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  UIKitDZ
+//
+//  Created by Анастасия Козлова on 22.09.2022.
+//
+
+import Foundation
+/// Hello
+struct Model {
+    let hello = "hello"
+}
--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -8,8 +8,76 @@
 import UIKit
 /// ViewController
 class ViewController: UIViewController {
+    
+    let helloLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .yellow
+        label.font = .systemFont(ofSize: 40)
+        label.numberOfLines = 0 
+        return label
+    }()
+    
+    let helloButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .yellow
+        button.setTitle("Начать", for: .normal)
+        button.isHighlighted = true
+        button.setTitleColor(UIColor.purple, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 30)
+        button.layer.cornerRadius = 100
+        button.layer.borderColor = UIColor.purple.cgColor
+        button.layer.borderWidth = 10
+        return button
+    }()
+    
+    let model = Model()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .black
+        
+        createButton()
+        createLabel()
+        
+        helloButton.addTarget(self, action: #selector(helloButtonAction(sender: )), for: .touchUpInside)
+    }
+    
+    func createButton() {
+        helloButton.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        helloButton.center = view.center
+        view.addSubview(helloButton)
+    }
+    
+    func createLabel() {
+        helloLabel.frame = CGRect(x: 0, y: 250, width: 150, height: 50)
+        helloLabel.center.x = view.center.x
+        view.addSubview(helloLabel)
+    }
+    
+    @objc func helloButtonAction(sender: UIButton) {
+        
+        let alertController = UIAlertController(
+            title: "Привет! ✌🏻",
+            message: "Введи слово",
+            preferredStyle: .alert
+        )
+        
+        let alertAction = UIAlertAction(
+            title: "Отправить",
+            style: .default
+        ) { _ in
+            let text = alertController.textFields?.first?.text ?? ""
+            if text == "leohl" {
+                self.helloLabel.text = self.model.hello
+            } else {
+                self.helloLabel.text = String(text.reversed())
+            }
+        }
+        
+        alertController.addTextField()
+        
+        alertController.addAction(alertAction)
+        present(alertController, animated: true)
     }
 }
