d95f94afd6e46738beb185ad6b05560c4fe3d8fd
Наташа Ананас <konnat7@mail.ru>
2022-09-27 12:26:18 +0300

    no message

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 /// ViewController
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var appPicker: UIPickerView!
+    
+    
+    let appArray = ["Share", "Alert Controller" ]
+    
+    var activityViewController = UIActivityViewController(activityItems: ["Apps"], applicationActivities: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    @IBAction func shareButtonAction(_ sender: Any) {
+        
+        print("Тут")
+        
+        self.present(activityViewController, animated: true, completion: nil)
     }
 }
