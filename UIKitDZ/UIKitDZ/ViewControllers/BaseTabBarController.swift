4a62b2303fe858b6792c60a9e500a82d80c722f9
Наташа Ананас <konnat7@mail.ru>
2022-09-30 13:22:39 +0300

    начало

---
+++ UIKitDZ/UIKitDZ/ViewControllers/BaseTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTabBarController.swift
+//  UIKitDZ
+//
+//  Created by Анастасия Козлова on 30.09.2022.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
