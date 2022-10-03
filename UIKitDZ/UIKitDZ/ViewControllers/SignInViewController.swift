02413fdc951d6458b129e30603c6778ce2484a4b
Наташа Ананас <konnat7@mail.ru>
2022-10-03 18:28:34 +0300

    Первый экран

---
+++ UIKitDZ/UIKitDZ/ViewControllers/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Анастасия Козлова on 21.09.2022.
+//
+
+import UIKit
+/// ViewController
+class SignInViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createUI()
+    }
+    
+    private func createUI() {
+        signInButton.layer.cornerRadius = 20
+        signInButton.clipsToBounds = true
+    }
+}
